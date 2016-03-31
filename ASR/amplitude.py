0a460ecbe218e0489629d4d8c63ebe36f6aa9370
Ray Shen <raybrshen@gmail.com>
2016-03-31 18:04:50 -0400

    Refactored asr codes.

---
+++ ASR/amplitude.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#
+# created by ray on 2016-03-16
+#
+# Function "list_devices" detects available audio input/output devices.
+#
+# Function "amplitude_testing" displays the input signal amplitude by calculating the
+# root mean square of the data chunk. Terminate the program with keyboard interrupt.
+#
+
+import pyaudio
+import audioop
+import sys
+import time
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 2048
+DEPTH = 16 # signal depth in bits
+DEVICE = 1 # for laptop's mic
+MAX_AMP = 800
+MAX_LINE = 50
+SLOPE = MAX_AMP/MAX_LINE
+# DEVICE = 3 # for lab's mic
+# SEC = 30
+
+
+#
+# amplitude testing
+#
+def print_debug(s):
+  sys.stdout.write('\r'+' '*80+'\r'+s)
+  sys.stdout.flush()
+
+def amplitude_testing():
+  audio = pyaudio.PyAudio()
+  stream = audio.open(format = FORMAT,
+                      channels = CHANNELS,
+                      rate = RATE,
+                      input = True,
+                      frames_per_buffer = CHUNK,
+                      input_device_index = DEVICE)
+  print '=> testing...'
+
+  # for i in xrange(SEC * RATE / CHUNK ):
+  #     data = stream.read(CHUNK)
+  #     print_debug(' | rms: '+str(audioop.rms(data, 2)))
+  #     time.sleep(0.01)
+  while True:
+    try:
+      data = stream.read(CHUNK)
+      rms = audioop.rms(data, DEPTH / 8)
+      progress = min(MAX_LINE,rms/SLOPE)
+      print_debug('['+'|'*progress+' '*(MAX_LINE-progress)+'] rms:'+str(rms))
+      time.sleep(0.01)
+    except KeyboardInterrupt:
+      print ; break
+
+  stream.stop_stream()
+  stream.close()
+  audio.terminate()
+  print '=> end'
+
+if __name__ == '__main__':
+  amplitude_testing()
--- ASR/test.py
+++ ASR/test.py
@@ -1,56 +1,10 @@
-import pyaudio
-import audioop
 import sys
 import time
-import wave
-import speech_recognition as sr
-from os import path
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 2048
-DEVICE = 3
-SEC = 30
-
-#
-# list device
-#
-# p = pyaudio.PyAudio()
-# info = p.get_host_api_info_by_index(0)
-# num_devices = info.get('deviceCount')
-# #for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
-# for i in range (0, num_devices):
-#     if p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
-#         print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name')
-#     if p.get_device_info_by_host_api_device_index(0,i).get('maxOutputChannels')>0:
-#         print "Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name')
-# dev_info = p.get_device_info_by_index(1)
-# print "Selected device is ", dev_info.get('name')
-# p.terminate()
-
-#
-# threshold testing
-#
-audio = pyaudio.PyAudio()
-stream = audio.open(format = FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    input = True,
-                    frames_per_buffer = CHUNK,
-                    input_device_index = DEVICE)
-print '=> testing...'
 
 def print_debug(s):
-    sys.stdout.write('\r                                      \r'+s)
+  sys.stdout.write('\r' + ' ' * 20 + '\r' + s)
+  sys.stdout.flush()
 
-for i in xrange(SEC * RATE / CHUNK ):
-    data = stream.read(CHUNK)
-    print_debug(' | rms: '+str(audioop.rms(data, 2)))
-    time.sleep(0.01)
-
-stream.stop_stream()
-stream.close()
-audio.terminate()
-print '\n=> end'
-
+for i in xrange(1,21):
+  print_debug('|'+'.'*i+' '*(20-i)+'| %d%%'%i)
+  time.sleep(0.2)
