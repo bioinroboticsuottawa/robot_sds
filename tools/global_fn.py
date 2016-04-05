8daed75821f71c0c2cf99fd7f64dbf484f320fee
Ray Shen <raybrshen@gmail.com>
2016-04-05 11:12:09 -0400

    Implemented enum type as a global tool.

--- tools/global_fn.py
+++ tools/global_fn.py
@@ -2,21 +2,29 @@
 #
 # created by Ray on 2016-03-31
 #
-# clean current line anyway
-# if given a string, print at the same line
+# Definition of global available functions.
 #
 
 import sys,time
 
-LINE_LEN = 80
 
+LINE_LEN = 80
 def print_debug(s=None):
   sys.stdout.write('\r' + ' ' * LINE_LEN + '\r')
   if s: sys.stdout.write(s)
   sys.stdout.flush()
   return
 
+def enum(*enums):
+  enums = dict(zip(enums,range(len(enums))))
+  return type('EnumType', (), enums)
+
+
 if __name__ == '__main__':
+  MODULE = enum('ACT','BBC','CVF')
+  print MODULE.ACT
+  print MODULE.CVF
+  print MODULE.BBC
   print_debug('abc')
   time.sleep(1)
   print_debug('cde')
